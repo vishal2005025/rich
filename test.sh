ccf2c34ac6748512c8c279f434bbf952835d1a2f
vishal2005025 <vishalmalyan2005@gmail.com>
2026-07-16 13:02:57 +0530

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$#" -ne 3 ] || [ "$1" != "--output_path" ]; then
     exit 2
 fi
 
 case "$3" in
     base)
-        exec python -m poetry run pytest \
+        exec poetry run pytest \
             tests/test_pretty.py \
             -k "not (test_pretty_repr_does_not_mutate_tuple_subclass or test_pretty_repr_does_not_leave_probe_attributes_on_auto_vivifying_object)" \
             --junitxml="$2"
         ;;
     new)
-        exec python -m poetry run pytest \
+        exec poetry run pytest \
             tests/test_pretty.py \
             -k "test_pretty_repr_does_not_mutate_tuple_subclass or test_pretty_repr_does_not_leave_probe_attributes_on_auto_vivifying_object" \
             --junitxml="$2"
